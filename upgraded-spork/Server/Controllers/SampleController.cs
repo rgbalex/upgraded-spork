f54ee4c6cf1900629c6c40749acaf76c7b3596c0
Alexander Angelidis <alex.m.angelidis@gmail.com>
2023-07-23 14:36:52 +0100

    Playing with Controllers and routing

---
+++ upgraded-spork/Server/Controllers/SampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace upgraded_spork.Server.Controllers
+{
+    // Seems to just make things complicated.
+    //[ApiController]
+    [Route("[controller]")]
+    public class SampleController : Controller
+    {
+        private readonly ILogger<SampleController> _logger;
+
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("Hello, World!");
+        }
+
+        [Route("zain")]
+        public JsonResult Zain()
+            => Json(new { Name = "Zain Ul Hassan", ID = 1 });
+
+        [Route("alex")]
+        public JsonResult Alex()
+            => Json(new { Name = "Alex", ID = 2 });
+
+    }
+}
