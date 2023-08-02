e0ffbe008355ee8ca28e91b0a6a222bba629d4a8
Alexander Angelidis <alex.m.angelidis@gmail.com>
2023-08-02 10:05:14 +0100

    Working JS example

---
+++ upgraded-spork/Client/Pages/Component.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace upgraded_spork.Client.Pages;
+
+public partial class Component
+{
+    private string sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+    //MudTextField<string> _logWindow;
+
+    private void _doubleWindowText()
+    {
+        sampleText = sampleText + "\n" + sampleText;
+
+        InvokeAsync(() => ShowAlertWindow());
+    }
+
+    async Task TextChanged(string text)
+    {
+        //await _logWindow.SelectRangeAsync(text.Length - 2, text.Length - 1);
+        StateHasChanged();
+    }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    private IJSObjectReference _jsModule;
+
+    public async Task ShowAlertWindow()
+    {
+        await _jsModule.InvokeVoidAsync("showAlert", "JS function called from .NET");
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/ScrollToBottom.js");
+    }
+}
+
---
+++ upgraded-spork/Client/wwwroot/scripts/ScrollToBottom.js
@@ -0,0 +1,13 @@
+﻿//function scrollToEnd(textarea) {
+//    //textarea.scrollTop = textarea.scrollHeight;
+//    alert("Sample Text")
+//}
+
+//function scrollLogToBottom() {
+//    logTa = document.getElementById("logTextArea")
+//    logTa.scrollTop = logTa.scrollHeight;
+//}
+
+export function showAlert(message) {
+    alert(message);
+}
